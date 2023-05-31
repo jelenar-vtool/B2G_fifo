e33317053430d119b8fae47fd0bf5118eae88818
Aleksandra Dimanic <aleksandrad@thevtool.com>
2023-05-31 16:05:56 +0300

    adding runme script

---
+++ runme_script/filelist_example.f
@@ -0,0 +1,14 @@
++incdir+${training_dir}/verif/env
+${training_dir}/verif/env/env_pkg.sv
+
++incdir+${training_dir}/verif/tests 
+${training_dir}/verif/tests/test_lib_pkg.sv
+
++incdir+${training_dir}/tb
+${training_dir}/tb/testbench.sv
+
++incdir+${training_dir}/rtl
+${training_dir}/rtl/design.sv
+
+
+
