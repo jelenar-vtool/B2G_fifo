01afa7715d846ec7921a2fb2d6075a58daf83c35
Aleksandra Dimanic <aleksandrad@thevtool.com>
2023-10-09 18:28:58 +0000

    adding the script to copy files from one lab to the other

--- verif_training/lab13/sv/env/b2gfifo_env_pkg.sv
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------------------------------------
-package b2gfifo_env_pkg;
-    import uvm_pkg::*;
-    import b2gfifo_pkg::*;
-    
-    `include "uvm_macros.svh" 
-    `include "b2gfifo_env_cfg.sv"
-    `include "b2gfifo_env.sv"
-endpackage 
